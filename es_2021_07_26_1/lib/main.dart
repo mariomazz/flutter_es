547faa2456a7e36227b2d9674aae17882bc52e9c
Mario <mario.mazzarelli@asfweb.net>
2021-07-27 12:27:47 +0200

    layout builder

--- es_2021_07_26_1/lib/main.dart
+++ es_2021_07_26_1/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:sizer/sizer.dart';
 
 void main() => runApp(Main());
@@ -22,67 +22,23 @@ class Main extends StatelessWidget {
   }
 }
 
-// ignore: public_member_api_docs
 class MyApp extends StatelessWidget {
-  // ignore: public_member_api_docs
-  MyApp({Key? key}) : super(key: key);
-  MediaQueryData? queryData;
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    queryData = MediaQuery.of(context);
-    print(queryData?.devicePixelRatio);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: AutoSizeText(
-          'App bars',
-          style: TextStyle(fontSize: 30.0),
-          maxLines: 1,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: queryData?.devicePixelRatio as double,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 4,
-                  ),
-                ),
-                child: const Center(
-                  child: AutoSizeText(
-                    'This string will be automatically resized to fit in two lines.',
-                    style: TextStyle(fontSize: 30.0),
-                    maxLines: 2,
-                  ),
-                ),
-              ),
-            ),
-            AspectRatio(
-              aspectRatio: queryData?.devicePixelRatio as double,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 4,
-                  ),
-                ),
-                child: AspectRatio(
-                  aspectRatio: 100 / 100,
-                  child: Image.network(
-                    'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return Container(
+            color: Colors.blueAccent,
+          );
+        } else {
+          return Container(
+            color: Colors.amberAccent,
+          );
+        }
+      }),
     );
   }
 }
