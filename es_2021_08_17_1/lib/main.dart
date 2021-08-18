7f81c6096abe6e5e0ccfedc09f660a64a5ce82dd
mario <mario.mazzarelli@asfweb.net>
2021-08-18 20:51:36 +0200

    ....

--- es_2021_08_17_1/lib/main.dart
+++ es_2021_08_17_1/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reciperlich',
-      theme: ThemeData.light(),
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: const RecipeListPage(),
     );
