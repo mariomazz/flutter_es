8f00a8a26cbb047e434134714a963c4dab958dc6
mario <mario.mazzarelli@asfweb.net>
2021-08-25 15:09:07 +0200

    ....

--- es_2021_08_23_1/lib/screens/home page/home_page.dart
+++ es_2021_08_23_1/lib/screens/home page/home_page.dart
@@ -1,7 +1,13 @@
+import 'package:chopper/chopper.dart';
+import 'package:es_2021_08_23_1/models/posts/post.dart';
+import 'package:es_2021_08_23_1/models/users/user.dart';
 import 'package:es_2021_08_23_1/screens/posts/posts_screen.dart';
 import 'package:es_2021_08_23_1/screens/users/users_screen.dart';
+import 'package:es_2021_08_23_1/services/posts/service_posts.dart';
+import 'package:es_2021_08_23_1/services/users/service_users.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -11,6 +17,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+
+    users = updateAndGetUsers();
+    posts = updateAndGetPosts();
+  }
+
+  Future<Response<List<Post_>>>? posts;
+  Future<Response<List<User>>>? users;
+
+  Future refreshPosts() async {
+    setState(() {
+      posts = updateAndGetPosts();
+    });
+  }
+
+  Future refreshUsers() async {
+    setState(() {
+      users = updateAndGetUsers();
+    });
+  }
+
+  Future<Response<List<Post_>>> updateAndGetPosts() async {
+    return await Provider.of<ServicePosts>(context, listen: false).getPosts();
+  }
+
+  Future<Response<List<User>>> updateAndGetUsers() async {
+    return await Provider.of<ServiceUsers>(context, listen: false).getUsers();
+  }
+
   int number = 0;
   increment() {
     setState(() {
@@ -50,12 +87,50 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           currentSelection == 0
-              ? Expanded(
-                  child: PostsScreen(),
+              ? FutureBuilder<Response<List<Post_>>>(
+                  future: posts,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<Response<List<Post_>>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Expanded(
+                        child: new Center(
+                          child: new CircularProgressIndicator(),
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return Center(
+                        child: new Text('Error: ${snapshot.error}'),
+                      );
+                    } else {
+                      final posts = snapshot.data!.body ?? <Post_>[];
+
+                      return PostsScreen(posts: posts);
+                    }
+                  },
                 )
-              : Expanded(
-                  child: UsersScreen(),
-                ),
+              : FutureBuilder<Response<List<User>>>(
+                  future: users,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<Response<List<User>>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Expanded(
+                        child: new Center(
+                          child: new CircularProgressIndicator(),
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return Center(
+                        child: new Text('Error: ${snapshot.error}'),
+                      );
+                    } else {
+                      final users = snapshot.data!.body ?? <User>[];
+
+                      return UsersScreen(users: users);
+                    }
+                  },
+                )
         ],
       ),
       floatingActionButton: ClipOval(
