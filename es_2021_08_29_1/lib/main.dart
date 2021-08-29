df30bad44ed84d316089722a1c1572bd1c2a8909
mario <mario.mazzarelli@asfweb.net>
2021-08-30 00:11:04 +0200

    .....

--- es_2021_08_29_1/lib/main.dart
+++ es_2021_08_29_1/lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:es_2021_08_29_1/routes/route_generator.dart';
+import 'package:es_2021_08_29_1/services/registration.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Foodybite',
-      theme: ThemeData(
-        textTheme:
-            GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme),
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return Provider(
+      create: (_) => RegistrationService.create(),
+      dispose: (_, RegistrationService service) => service.client.dispose(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Foodybite',
+        theme: ThemeData(
+          textTheme:
+              GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme),
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/',
+        onGenerateRoute: RouteGenerator.generateRoute,
       ),
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
---
+++ es_2021_08_29_1/lib/services/registration.dart
@@ -0,0 +1,23 @@
+import 'package:chopper/chopper.dart';
+
+part 'registration.chopper.dart';
+
+@ChopperApi(baseUrl: '/registered-users')
+abstract class RegistrationService extends ChopperService {
+  @Post()
+  Future<Response> registrationRequest(
+    @Body() Map<String, dynamic> body,
+  );
+
+  static RegistrationService create() {
+    final client = ChopperClient(
+      baseUrl: 'http://localhost',
+      services: [
+        _$RegistrationService(),
+      ],
+      converter: JsonConverter(),
+    );
+
+    return _$RegistrationService(client);
+  }
+}
